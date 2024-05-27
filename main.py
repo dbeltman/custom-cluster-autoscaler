1c284713d92e6868467be360794ab284522601c3
dbeltman <daan.w.beltman@gmail.com>
2024-05-27 23:06:34 +0200

    refactor some stuff and comment (AI generated)

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import logging
 import os
 import time
 from src.classes import all_reasons, PendingPodReason, NodeCapabilities, AutoScaleNode
-from src.kubernetes_handler import handle_pending_pods
-from src.node_handler import find_node_by_capability
-from src.node_handler import handle_node_request
+from pod_handler import handle_pending_pods
+from inventory_handler import find_node_by_capability
+from inventory_handler import handle_node_request
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -29,7 +29,8 @@ def main():
         for pending_pod in pending_pods:
             # print(pending_pod.reason.requirement, "needed!")
             print(find_node_by_capability(pending_pod.reason.requirement))
-            node = find_node_by_capability(pending_pod.reason.requirement)[0]
+            # Note: Hardcoding the first-in-list option is not desired. A better approach would be to handle all found nodes.
+            node = find_node_by_capability(pending_pod.reason.requirement)[0]  # [0] indexing is used here, which may lead to issues if there are multiple suitable nodes
             handle_node_request(nodename=node["nodeName"])
     else:
         logger.info("No pending pods as of now")
