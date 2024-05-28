c5ea20257448901e4a2e18239d15372f6f971099
dbeltman <daan.w.beltman@gmail.com>
2024-05-28 23:04:21 +0200

    logging for main, fix function call with asyncio

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import logging
 import os
 import time
+import asyncio
 from src.classes import all_reasons, PendingPodReason, NodeCapabilities, AutoScaleNode
-from kubernetes_handler import get_pending_pods, check_node_presence
-from inventory_handler import get_nodes_by_requirement
-from bmc_handler import power_on_esphome_system
+from src.kubernetes_handler import get_pending_pods, check_node_presence
+from src.inventory_handler import get_nodes_by_requirement
+from src.bmc_handler import power_on_esphome_system
 # Set up logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -26,17 +27,22 @@ def main():
     pending_pods = get_pending_pods()
     if len(pending_pods) > 0:
         for pending_pod in pending_pods:
-            # print(pending_pod.reason.requirement, "needed!")
-            print(get_nodes_by_requirement(pending_pod.reason.requirement))
-            # Note: Hardcoding the first-in-list option is not desired. A better approach would be to handle all found nodes.
             for node in get_nodes_by_requirement(pending_pod.reason.requirement):  # Loop through each node by requirement
                 node_status = check_node_presence(node.node_name)   # Check node presence
                 if node_status == True:
                         logger.info(f"{node.node_name} is present, skipping auto-scaling.")  # Skip this node if present
                         continue
                 else:
-                        power_on_esphome_system(node.node_name)
+                        logger.info(f"{node.node_name} is not present, turning on the node.")
+                        # Check BMC method and perform action for esphome
+                        if node.bmc_method == "esphome":
+                            logger.info(f"Turning on the {node.node_name} using esphome system.")
+                            asyncio.run(power_on_esphome_system(node.node_name))
+                        else:
+                            logger.warning(f"No mechanism for BMC method '{node.bmc_method}' yet!")
+                        
                         break  # If the node is not present, proceed with auto-scaling and break the loop
+                
     else:
         logger.info("No pending pods as of now")
 
@@ -44,4 +50,4 @@ def main():
 if __name__ == "__main__":
     while True:
         main()
-        time.sleep(5)
+        time.sleep(30)
