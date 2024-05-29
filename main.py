a55eaff089aee13ba237addf4f3ae241b9d23538
dbeltman <daan.w.beltman@gmail.com>
2024-05-29 22:18:54 +0200

    docstrings

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from src.classes import all_reasons, PendingPodReason, NodeCapabilities, AutoSca
 from src.kubernetes_handler import get_pending_pods, check_node_presence
 from src.inventory_handler import get_nodes_by_requirement
 from src.bmc_handler import power_on_esphome_system
+
 # Set up logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -15,7 +16,8 @@ ch = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 ch.setFormatter(formatter)
 logger.addHandler(ch)
-# Set up kubernetes
+
+
 if os.getenv("PRODUCTION") == "True":
     logger.info("Running in PRODUCTION mode!")
 else:
@@ -25,14 +27,25 @@ else:
     debugpy.listen(5679)
 
 def signal_handler(signum, frame):
+    """
+    This function handles the SIGTERM signal to gracefully shutdown the application and logs a message indicating that it is stopping.
+
+    Args:
+        signum (int): Signal number, which is 15 for SIGTERM.
+        frame (FrameType): Frame object containing information about the current call stack.
+    """
     logger.info('Caught SIGTERM signal. Stopping...')
     sys.exit(0)
-
-
-
+    
 def main():
+    """
+    Auto-scales nodes based on pending pods in Kubernetes cluster.
+    If there are pending pods, it checks for available nodes in the inventory that meet the required conditions.
+    If a node is not present, it turns on the node using the specified BMC method (esphome by default).
+    """
     signal.signal(signal.SIGTERM, signal_handler)
     pending_pods = get_pending_pods()
+
     if len(pending_pods) > 0:
         for pending_pod in pending_pods:
             for node in get_nodes_by_requirement(pending_pod.reason.requirement):  # Loop through each node by requirement
@@ -50,11 +63,8 @@ def main():
                             logger.warning(f"No mechanism for BMC method '{node.bmc_method}' yet!")
                         
                         break  # If the node is not present, proceed with auto-scaling and break the loop
-                
     else:
         logger.info("No pending pods as of now")
-
-
 if __name__ == "__main__":
     while True:
         main()
