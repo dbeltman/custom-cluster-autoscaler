7b87819730dbe8f46dd7c8d118eed75529b62e7e
dbeltman <daan.w.beltman@gmail.com>
2025-03-22 01:01:16 +0100

    mqtt interface

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import sys
 import signal
 import asyncio
 from src.classes import all_reasons, PendingPodReason, NodeCapabilities, AutoScaleNode, PendingPod
-from src.kubernetes_handler import check_node_presence_in_cluster, label_pod_with_custom_autoscaler_trigger, get_pods_on_node
-from src.inventory_handler import get_nodes_by_requirement, get_requirements_by_node
-from src.bmc_handler import power_on_esphome_system
+from src.kubernetes_handler import check_node_presence_in_cluster, label_pod_with_custom_autoscaler_trigger
+from src.inventory_handler import get_nodes_by_requirement
+from src.bmc_handler import power_on_esphome_system, power_on_mqtt_system
 from src.event_parser import handle_event
 from kubernetes import client, config, watch
 # Set up logging
@@ -45,7 +45,7 @@ def main():
     If a node is not present, it turns on the node using the specified BMC method (esphome by default).
     """
     signal.signal(signal.SIGTERM, signal_handler)
-    handle_pending_pods()
+    watch_pending_pods()
 
 def setup_kubernetes_client():
     # Set up kubernetes
@@ -69,12 +69,62 @@ def setup_kubernetes_client():
     configuration.ssl_ca_cert = capath
     return configuration
 
-def handle_pending_pods():
+def handle_pending_pod(event):
+    pendingpodreason = handle_event(event["object"].message)
+    obj=event['object'].involved_object
+    pod = v1.read_namespaced_pod(name=obj.name, namespace=obj.namespace)
+    if "cluster-autoscaler-triggered" in pod.metadata.labels:
+        logger.info(f"Found already handled pod: {pod.metadata.name} in namespace {pod.metadata.namespace}")
+        return None                   
+    logger.info(
+        pendingpodreason.message
+        + " | "
+        + pendingpodreason.name
+        + ": "
+        + obj.name
+    )
+    pending_pod = PendingPod(pendingpodreason, obj.name, obj.namespace)
+    if pendingpodreason.name != "Unknown":
+        matching_nodes=get_nodes_by_requirement(pending_pod.reason.requirement)
+        if len(matching_nodes) > 0:
+            for node in matching_nodes:  # Loop through each node by requirement
+                if check_node_presence_in_cluster(node.node_name) == True:
+                        logger.info(f"{node.node_name} is present, skipping auto-scaling and trying to find another node.")  # Skip this node if present
+                        matching_nodes.remove(node)
+                        if len(matching_nodes) > 0:
+                            continue
+                        else:
+                            logger.warning("No more matching nodes left that match this pending requirement")
+                            label_pod_with_custom_autoscaler_trigger(pending_pod.podname, pending_pod.podnamespace)
+
+                else:
+                        logger.info(f"{node.node_name} is not present, turning on the node.")
+                        # Check BMC method and perform action for esphome
+                        if node.bmc_method == "esphome":
+                            logger.info(f"Turning on the {node.node_name} using esphome system.")
+                            if asyncio.run(power_on_esphome_system(node.node_name)) != False:
+                                label_pod_with_custom_autoscaler_trigger(pending_pod.podname, pending_pod.podnamespace)
+                            else:
+                                logger.error(f"Function did not return True, something went wrong powerin on node {node.node_name}")
+                        elif node.bmc_method == "mqtt":
+                            if power_on_mqtt_system(nodename=node.node_name):
+                                label_pod_with_custom_autoscaler_trigger(pending_pod.podname, pending_pod.podnamespace)
+                            else:
+                                logger.error(f"Function did not return True, something went wrong powerin on node {node.node_name}")
+                        else:
+                            logger.warning(f"No mechanism for BMC method '{node.bmc_method}' yet!")
+                            label_pod_with_custom_autoscaler_trigger(pending_pod.podname, pending_pod.podnamespace)
+
+                        break  # If the node is not present, proceed with auto-scaling and break the loop
+        else:
+            logger.error(f"No matching nodes found for {pending_pod.reason.requirement}")  
+    else:
+        logger.warning("Pod has unknown pending reason")
+        return None
+def watch_pending_pods():
     start_time = datetime.datetime.now(datetime.timezone.utc)
-    configuration = setup_kubernetes_client()
-    v1 = client.CoreV1Api(client.ApiClient(configuration))
     w = watch.Watch()
-    for event in w.stream(v1.list_event_for_all_namespaces,):
+    for event in w.stream(v1.list_event_for_all_namespaces):
         if event["object"].reason == "FailedScheduling" and event["object"].message.startswith("skip schedule deleting pod") == False and event["type"] == "ADDED":
             event_time=event["object"].event_time
             if (start_time - event_time).total_seconds() > 2:
@@ -83,44 +133,13 @@ def handle_pending_pods():
             print ("Event has failedscheduling reason:")
             # print("Event: %s %s %s" % (event['object'].reason, event['object'].message, event['object'].involved_object.name))
             try:
-                pendingpodreason = handle_event(event["object"].message)
-                obj=event['object'].involved_object
-                pod = v1.read_namespaced_pod(name=obj.name, namespace=obj.namespace)
-                if "cluster-autoscaler-triggered" in pod.metadata.labels:
-                    logger.info(f"Found already handled pod: {pod.metadata.name} in namespace {pod.metadata.namespace}")
-                    continue                    
-                logger.info(
-                    pendingpodreason.message
-                    + " | "
-                    + pendingpodreason.name
-                    + ": "
-                    + obj.name
-                )
-                pending_pod = PendingPod(pendingpodreason, obj.name, obj.namespace)
-                if pendingpodreason.name != "Unknown":
-                    matching_nodes=get_nodes_by_requirement(pending_pod.reason.requirement)
-                    if len(matching_nodes) > 0:
-                        for node in matching_nodes:  # Loop through each node by requirement
-                            node_status = check_node_presence_in_cluster(node.node_name)   # Check node presence
-                            if node_status == True:
-                                    logger.info(f"{node.node_name} is present, skipping auto-scaling and trying to find another node.")  # Skip this node if present
-                                    continue
-                            else:
-                                    logger.info(f"{node.node_name} is not present, turning on the node.")
-                                    # Check BMC method and perform action for esphome
-                                    if node.bmc_method == "esphome":
-                                        logger.info(f"Turning on the {node.node_name} using esphome system.")
-                                        if asyncio.run(power_on_esphome_system(node.node_name)) != False:
-                                            label_pod_with_custom_autoscaler_trigger(pending_pod.podname, pending_pod.podnamespace)
-                                    else:
-                                        logger.warning(f"No mechanism for BMC method '{node.bmc_method}' yet!")
-                                        label_pod_with_custom_autoscaler_trigger(pending_pod.podname, pending_pod.podnamespace)
-
-                                    break  # If the node is not present, proceed with auto-scaling and break the loop
-                    else:
-                        logger.error(f"No matching nodes found for {pending_pod.reason.requirement}")
+                handle_pending_pod(event=event)
             except TypeError as e:
                 print(f'ERROR: {e}')
+
+configuration = setup_kubernetes_client()
+v1 = client.CoreV1Api(client.ApiClient(configuration))
+
 if __name__ == "__main__":
     while True:
         main()
